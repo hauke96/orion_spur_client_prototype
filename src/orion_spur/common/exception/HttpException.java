48647d8e7632893f3b808b3d8874b8a9174660b0
Hauke Stieler <mail@hauke-stieler.de>
2017-08-23 19:57:43 +0200

    Add exception message when creating HttpException

--- src/orion_spur/common/exception/HttpException.java
+++ src/orion_spur/common/exception/HttpException.java
@@ -6,15 +6,15 @@ import juard.contract.Contract;
 
 public class HttpException extends Exception {
 	private int _statusCode;
-	private String _message;
 
 	public HttpException(int statusCode, String message)
 	{
+		super("Exited with HTTP status code "+statusCode+". Details: "+message);
+		
 		Contract.Satisfy(statusCode >= 0);
 		Contract.NotNullOrEmpty(message);
 		
 		_statusCode = statusCode;
-		_message = message;
 	}
 	
 	public int getStatusCode()
