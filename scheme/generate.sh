541d81ed8dbf8996139cc5465d4bdb2ebdd2bfb9
Hauke Stieler <mail@hauke-stieler.de>
2017-12-21 00:58:36 +0100

    Edit generate.sh from 'material' to 'generated' namespace

--- scheme/generate.sh
+++ scheme/generate.sh
@@ -1,2 +1,2 @@
-java -jar json2code.jar orion_scheme.json java ../client/src/orion_spur/common/material/ orion_spur.common.material
-java -jar json2code.jar orion_scheme.json go ../server/src/material material
+java -jar json2code.jar orion_scheme.json java ../client/src/orion_spur/common/generated/ orion_spur.common.generated
+java -jar json2code.jar orion_scheme.json go ../server/src/generated generated
